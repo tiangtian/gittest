58f5f267f5e00360359c12c0864a7eaae64223f4
xiamu <1402144183@qq.com>
2018-07-11 16:17:42 +0800

    第三次提交

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,4 +2,13 @@
  * Created by Administrator on 2018/7/11.
  */
 public class Test {
+    private  String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
